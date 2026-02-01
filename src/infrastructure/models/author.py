6ed3b69999c3df140b00643f07e71ec8d0d638bd
cdxy <ogxen@mail.ru>
2026-02-02 00:14:05 +0300

    Add alembic and author repo implementation

--- src/infrastructure/models/author.py
+++ src/infrastructure/models/author.py
@@ -1,4 +1,4 @@
-from uuid import UUID
+from uuid import UUID, uuid4
 
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -8,6 +8,6 @@ from src.infrastructure.models.base import BaseOrmModel
 class AuthorModel(BaseOrmModel):
     __tablename__ = "author"
 
-    author_id: Mapped[UUID] = mapped_column(UUID, primary_key=True)
+    author_id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default_factory=uuid4)
     name: Mapped[str] = mapped_column(str, nullable=False)
     type: Mapped[str] = mapped_column(str, nullable=False)
--- src/infrastructure/repository/author_repository_impl.py
+++ src/infrastructure/repository/author_repository_impl.py
@@ -1,4 +1,17 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.application.dto.author_dto import AuthorDTO
+from src.infrastructure.models.author import AuthorModel
 from src.application.repository.contract import IRepository
 
 
-class AuthorRepository(IRepository): ...
+class AuthorRepository(IRepository):
+    def __init__(self, session: AsyncSession):
+        self._session = session
+        
+    def create(self, author: AuthorDTO):
+        author = AuthorModel(
+            name=AuthorDTO.name,
+            type=AuthorDTO.type,
+        )
+        self._session.add(author)
