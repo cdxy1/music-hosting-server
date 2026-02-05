42676e6df05d0a2c132aba9599493abd606776e8
cdxy1 <dalyvinchi@gmail.com>
2026-02-06 00:55:32 +0300

    Add graceful shutdown for db

--- src/setup/composition_root.py
+++ src/setup/composition_root.py
@@ -2,13 +2,17 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+# from src.infrastructure.config.postgres import PostgresConfig
+# from src.infrastructure.database.postgres import PostgresDatabase
 from src.presetntation.http.routes.root import get_root_router
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(app: FastAPI):    
+    # database_config = PostgresConfig()
+    # database = PostgresDatabase(database_config)
     yield
-    
+    # database.close_all_connections()
 
 app = FastAPI(lifespan=lifespan)
 
