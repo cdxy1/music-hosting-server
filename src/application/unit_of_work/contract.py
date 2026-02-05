61618d09aca838b8cb0252ca695a16d73b2158f8
cdxy1 <dalyvinchi@gmail.com>
2026-02-05 22:19:55 +0300

    Del comments

--- src/application/unit_of_work/contract.py
+++ src/application/unit_of_work/contract.py
@@ -4,23 +4,6 @@ from contextlib import asynccontextmanager
 
 
 class IUnitOfWork(ABC):
-    # @abstractmethod
-    # async def __aenter__(self) -> Self: ...
-
-    # @abstractmethod
-    # async def __aexit__(
-    #     self,
-    #     exc_type: type[BaseException] | None,
-    #     exc_val: BaseException | None,
-    #     exc_tb: Any,
-    # ): ...
-
-    # @abstractmethod
-    # async def commit(self): ...
-
-    # @abstractmethod
-    # async def rollback(self): ...
-
     @abstractmethod
     @asynccontextmanager
     async def __call__(self): ...
