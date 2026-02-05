3e821332e522f189491f898baba97748a33d442a
cdxy1 <dalyvinchi@gmail.com>
2026-02-05 22:18:35 +0300

    Add lifespan

--- src/setup/composition_root.py
+++ src/setup/composition_root.py
@@ -1,3 +1,15 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from src.presetntation.http.routes.root import get_root_router
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    
+
+app = FastAPI(lifespan=lifespan)
+
+root_router = get_root_router()
+app.include_router(root_router)
