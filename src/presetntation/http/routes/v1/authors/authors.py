bfc50346b125638c1a7b2f971705e29c499e24bb
cdxy1 <dalyvinchi@gmail.com>
2026-02-05 23:56:14 +0300

    Inject usecase into author post route

--- src/presetntation/http/routes/v1/authors/authors.py
+++ src/presetntation/http/routes/v1/authors/authors.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from src.presetntation.http.schemas.author import CreateAuthorRequest, CreateAuthorResponse
+from src.application.usecases.create_author_usecase import CreateAuthorUsecase
+from src.presetntation.http.dependencies.author_usecases import get_create_author_usecase
 
 router = APIRouter(prefix="/authors", tags=["authors"])
 
 @router.get("/")
-async def get_authors():
-    ...
+async def get_authors(author: CreateAuthorRequest, usecase: CreateAuthorUsecase = Depends(get_create_author_usecase)) -> CreateAuthorResponse:
+    usecase()
 
 @router.post("/")
 async def create_author():
