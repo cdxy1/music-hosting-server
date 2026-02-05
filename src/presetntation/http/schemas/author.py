daa78f93b6a58d0031edec618f06132fe02553a3
cdxy1 <dalyvinchi@gmail.com>
2026-02-05 23:26:17 +0300

    Add author pydantic schemas

--- src/presetntation/http/schemas/author.py
+++ src/presetntation/http/schemas/author.py
@@ -1,7 +1,12 @@
+from uuid import UUID
+
 from pydantic import BaseModel
 
 class CreateAuthorRequest(BaseModel):
-    ...
+    name: str
+    type: str
     
 class CreateAuthorResponse(BaseModel):
-    ...
+    id: UUID
+    name: str
+    type: str
