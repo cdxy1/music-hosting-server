bff8a688b00d739c0fa87af6865ab8375ac029cc
cdxy1 <dalyvinchi@gmail.com>
2026-02-05 22:42:17 +0300

    Upd author usecase

--- src/application/usecases/create_author_usecase.py
+++ src/application/usecases/create_author_usecase.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from src.application.dto.author_dto import AuthorDTO
 from src.application.repository.contract import IRepository
 from src.application.unit_of_work.contract import IUnitOfWork
@@ -5,10 +7,11 @@ from src.application.unit_of_work.contract import IUnitOfWork
 
 class CreateAuthorUsecase:
     
-    def __init__(self, repo: IRepository, uow: IUnitOfWork):
+    def __init__(self, repo: IRepository, uow_factory: Callable[[], IUnitOfWork]):
         self.repo = repo
-        self.uow = uow     
+        self.uow_factory = uow_factory     
 
-    async def __call__(self, author: AuthorDTO):        
-        async with self.uow() as session:
+    async def __call__(self, author: AuthorDTO):
+        uow = self.uow_factory()    
+        async with uow() as session:
             created_author = self.repo().create(session, author)
