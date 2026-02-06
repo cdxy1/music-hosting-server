cea38004e1812b717f7e022278c5d9dfbef60832
cdxy1 <dalyvinchi@gmail.com>
2026-02-06 22:59:57 +0300

    Rewrite uow factory, maybe it's awful but i don't care

--- src/infrastructure/unit_of_work/unit_of_work_factory.py
+++ src/infrastructure/unit_of_work/unit_of_work_factory.py
@@ -1,8 +1,21 @@
 from src.infrastructure.database.contract import IDatabase
 from src.infrastructure.unit_of_work.unit_of_work_impl import UnitOfWork
 
+class UnitOfWorkSingletonFactory:
+    _instance = None
+    _initialized = False
+    
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    def __init__(self, database: IDatabase = None):
+        if self._initialized:
+            return
+        self.database = database
+        self._initialized = True
+    
+    def create_uow_instance(self):
+        return UnitOfWork(self.database)
 
-def uow_factory(database: IDatabase):
-    def inner():
-        return UnitOfWork(database)
-    return inner
