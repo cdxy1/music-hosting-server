be43f8451df04207faebdf6dd452e759edf239c7
cdxy1 <dalyvinchi@gmail.com>
2026-02-08 15:14:03 +0300

    Del migration uri

--- src/infrastructure/config/postgres.py
+++ src/infrastructure/config/postgres.py
@@ -16,13 +16,3 @@ class PostgresConfig(IDatabaseConfig):
         db_name = os.environ.get("DB_NAME")
         
         return f"postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
-    
-    @property
-    def migration_database_uri(self) -> str:
-        db_host = os.environ.get("DB_HOST")
-        db_port = os.environ.get("DB_PORT")
-        db_user = os.environ.get("DB_USER")
-        db_password = os.environ.get("DB_PASSWORD")
-        db_name = os.environ.get("DB_NAME")
-        
-        return f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"        
