26aa7062defaf23cb8892b3c1ea85974dafc702b
cdxy1 <dalyvinchi@gmail.com>
2026-02-15 14:44:34 +0300

    Upd refactor author usecase structure

--- src/presentation/http/dependencies/author_usecases.py
+++ src/presentation/http/dependencies/author_usecases.py
@@ -1,7 +1,7 @@
-from src.application.usecases.create_author_usecase import CreateAuthorUsecase
-from src.application.usecases.delete_author_usecase import DeleteAuthorUsecase
-from src.application.usecases.get_all_authors import GetAllAuthorUsecase
-from src.application.usecases.get_author_usecase import GetAuthorUsecase
+from src.application.usecases.author.create_author_usecase import CreateAuthorUsecase
+from src.application.usecases.author.delete_author_usecase import DeleteAuthorUsecase
+from src.application.usecases.author.get_all_authors import GetAllAuthorUsecase
+from src.application.usecases.author.get_author_usecase import GetAuthorUsecase
 from src.infrastructure.repository.author_repository_impl import AuthorRepository
 from src.infrastructure.unit_of_work.unit_of_work_factory import (
     UnitOfWorkSingletonFactory,
