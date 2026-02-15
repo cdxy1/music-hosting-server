b92d0e7a5c1fc1f99b753e4c2623334422ce91d0
cdxy1 <dalyvinchi@gmail.com>
2026-02-16 01:37:02 +0300

    Impl Release entity

---
+++ src/domain/entities/release.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+from dataclasses import dataclass, field
+
+from src.domain.entities.author import Author
+from src.domain.entities.genre import Genre
+from src.domain.entities.track import Track
+
+@dataclass(slots=True, frozen=True)
+class Release:
+    name: str
+    author: Author
+    genre: Genre
+    tracks: list[Track]
+    release_data: datetime = field(default_factory=datetime.now)
+    id: UUID = field(default_factory=uuid4) 
