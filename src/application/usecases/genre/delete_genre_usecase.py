b3ee49f3727e7c04897d87cfd0c423fafdabcf72
cdxy1 <dalyvinchi@gmail.com>
2026-02-20 20:00:37 +0300

    Impl delete genre usecase

---
+++ src/application/usecases/genre/delete_genre_usecase.py
@@ -0,0 +1,12 @@
+from uuid import UUID
+
+from src.application.usecases.base import BaseUsecase
+
+
+class DeleteGenreUsecase(BaseUsecase):
+    async def __call__(self, genre_id: UUID):
+        uow = self.uow_factory()
+        async with uow() as session:
+            await self.repo.delete(session, genre_id)
+            
+            return genre_id
