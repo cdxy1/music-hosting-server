359a35b4ebb8511ce4e56952749aa3a6bb3b0782
cdxy1 <dalyvinchi@gmail.com>
2026-02-20 03:41:51 +0300

    Add mapper to base model

--- src/infrastructure/models/base.py
+++ src/infrastructure/models/base.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import DeclarativeBase
 
 
-class BaseOrmModel(DeclarativeBase): ...
+class BaseOrmModel(DeclarativeBase):
+    def to_dict(self):
+        return {key: value for key, value in self.__dict__.items() if key in self.__table__.columns.keys()}
