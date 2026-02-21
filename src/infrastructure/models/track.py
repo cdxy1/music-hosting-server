98aa97aa1b5cca14ef2f5a5da24dade745ad6ecc
cdxy1 <dalyvinchi@gmail.com>
2026-02-21 17:00:58 +0300

    Upd track model

--- src/infrastructure/models/track.py
+++ src/infrastructure/models/track.py
@@ -12,8 +12,8 @@ class TrackModel(BaseOrmModel):
     track_id: Mapped[UUID] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(nullable=False, unique=True)
     duration: Mapped[int]
-    audio_key: Mapped[str] = Mapped(unique=True)
-    image_key: Mapped[str] = Mapped(unique=True)
+    audio_key: Mapped[str] = mapped_column(unique=True)
+    image_key: Mapped[str] = mapped_column(unique=True)
     release_fk: Mapped[UUID] = mapped_column(ForeignKey("release.release_id"))
     
     release = relationship("ReleaseModel", back_populates="tracks", uselist=False, lazy="selectin")
