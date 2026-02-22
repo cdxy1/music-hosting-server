0d424cf818d0edb8d2e34be72a1be8b172498807
cdxy1 <dalyvinchi@gmail.com>
2026-02-23 00:33:36 +0300

    Add autodiscover to celery app and init s3

--- src/infrastructure/background_tasks/app.py
+++ src/infrastructure/background_tasks/app.py
@@ -1,7 +1,13 @@
 from celery import Celery
 
 from src.infrastructure.config.rabbitmq import RabbitMQConfig
+from src.infrastructure.database.s3.database import S3Storage
+from src.infrastructure.config.s3 import S3Config
 
 broker = RabbitMQConfig()
 
-app = Celery("worker", broker=broker.broker_uri, backend=broker.celery_backend)
+celery_app = Celery("worker", broker=broker.broker_uri, backend=broker.celery_backend)
+celery_app.autodiscover_tasks(["src.infrastructure.background_tasks.tasks"])
+
+config = S3Config()
+storage = S3Storage(config)
