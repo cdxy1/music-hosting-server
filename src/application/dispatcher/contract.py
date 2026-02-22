b0423a15d5498c91fb2001dcd2304af5839be6eb
cdxy1 <dalyvinchi@gmail.com>
2026-02-23 00:34:13 +0300

    Add task dispatcher

---
+++ src/application/dispatcher/contract.py
@@ -0,0 +1,5 @@
+from typing import Protocol
+
+class IDispatcher(Protocol):
+    def upload_file(self, file_key, file_data):
+        ...
---
+++ src/infrastructure/background_tasks/dispatcher.py
@@ -0,0 +1,8 @@
+from src.infrastructure.background_tasks.app import celery_app
+
+class TasksDispatcher:
+    def dispatch_upload_file(self, file_key, file_data):
+        celery_app.send_task(
+            "tasks.upload_file",
+            args=[file_key, file_data]
+        )
