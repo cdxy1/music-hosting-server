ea588faf5f0984a6d0d2f2ca43887e9c08e95985
cdxy1 <dalyvinchi@gmail.com>
2026-02-24 00:13:42 +0300

    Add models to __init__

---
+++ src/infrastructure/models/__init__.py
@@ -0,0 +1,4 @@
+from src.infrastructure.models.author import AuthorModel #NOQA
+from src.infrastructure.models.genre import GenreModel #NOQA
+from src.infrastructure.models.release import ReleaseModel #NOQA
+from src.infrastructure.models.track import TrackModel #NOQA
