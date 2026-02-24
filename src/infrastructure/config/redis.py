7835e899ff683604a9352aeba45a3763e4b9b1c5
cdxy1 <dalyvinchi@gmail.com>
2026-02-25 00:58:47 +0300

    Add redis config and refactor impl

---
+++ src/infrastructure/config/redis.py
@@ -0,0 +1,17 @@
+import os
+
+from src.infrastructure.config.contract import IDatabaseConfig
+
+class RedisConfig(IDatabaseConfig):
+    @property
+    def database_uri(self) -> str:
+        cache_host = os.environ.get("REDIS_HOST")
+        cache_port = os.environ.get("REDIS_PORT")
+        
+        return f"redis://{cache_host}:{cache_port}"
+
+    @property
+    def ttl_seconds(self):
+        cache_seconds_ttl = os.environ.get("REDIS_SECONDS_TTL", 60)
+
+        return int(cache_seconds_ttl)
--- src/infrastructure/database/redis/database.py
+++ src/infrastructure/database/redis/database.py
@@ -1,34 +1,40 @@
-from datetime import timedelta
 from typing import Optional
 
-from redis import ConnectionError, Redis
+from redis import ConnectionError
 from redis import asyncio as aioredis
 
 from src.infrastructure.config.contract import IDatabaseConfig
 
 
 class RedisCache:
+    _instance = None
     _connection = None
+    _config = None
     
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
             cls._instance = super().__new__(cls)
         return cls._instance
     
-    def __init__(self, config: IDatabaseConfig):
-        self.config = config
-        self._connection: Optional[Redis] = None
-
+    def __init__(self, config: IDatabaseConfig = None):
+        if not self._instance and not config:
+            raise
+        
+        if config:
+            self._config = config
+        
     async def connect(self):
-        self._connection = await aioredis.from_url(self.config.database_uri, decode_responses=True)
+        self._connection = await aioredis.from_url(self._config.database_uri, decode_responses=True)
 
     async def close(self):
         if self._connection:
             await self._connection.close()
 
-    async def set(self, key: str, value: str, expire: timedelta):
+    async def set(self, key: str, value: str, seconds_to_expire=None):
         try:
-            await self._connection.setex(key, expire, value)
+            ttl = seconds_to_expire if seconds_to_expire else self._config.ttl_seconds
+            
+            await self._connection.setex(key, ttl, value)
         except ConnectionError:
             raise
 
