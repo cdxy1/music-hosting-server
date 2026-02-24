f3c2a878d037ee426661330504d3d9f50529f419
cdxy1 <dalyvinchi@gmail.com>
2026-02-25 01:00:24 +0300

    Upd base usecase class

--- src/application/usecases/base.py
+++ src/application/usecases/base.py
@@ -2,9 +2,11 @@ from typing import Callable
 
 from src.application.repository.contract import IRepository
 from src.application.unit_of_work.contract import IUnitOfWork
+from src.application.cache.contract import ICacheWrapper
 
 
 class BaseUsecase:
-    def __init__(self, repo: IRepository, uow_factory: Callable[[], IUnitOfWork]):
+    def __init__(self, repo: IRepository, uow_factory: Callable[[], IUnitOfWork], cache: ICacheWrapper):
         self.repo = repo
-        self.uow_factory = uow_factory   
+        self.uow_factory = uow_factory
+        self.cache = cache
