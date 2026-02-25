63ef48126ffa8d9b7b4f3d674dc7bbe4bc4bc9a0
cdxy1 <dalyvinchi@gmail.com>
2026-02-25 22:44:37 +0300

    Add type to contract

--- src/application/cache/contract.py
+++ src/application/cache/contract.py
@@ -1,8 +1,8 @@
 from abc import ABC
-from typing import Callable
+from typing import Callable, Any
 
 class ICacheWrapper(ABC):
-    async def get_or_create(self, key, fn: Callable):
+    async def get_or_create(self, key, fn: Callable) -> Any:
         ...
     
     async def invalidate_cache(self, keys):
