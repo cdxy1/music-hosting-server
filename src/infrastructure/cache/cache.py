c02863c0656804c2f860f4e671fdc05d93f9ae36
cdxy1 <dalyvinchi@gmail.com>
2026-02-25 22:43:54 +0300

    Temp remove try except

--- src/infrastructure/cache/cache.py
+++ src/infrastructure/cache/cache.py
@@ -9,20 +9,16 @@ class CacheWrapper:
         self.cache = cache
         
     async def get_or_create(self, key, fn: Callable):
-        try:
-            if cached_data := await self.cache.get(key):
-                data = json.loads(cached_data)
-                deserialized_data = deserialize_json_to_dto(data)
-                return deserialized_data
+        if cached_data := await self.cache.get(key):
+            deserialized_data = deserialize_json_to_dto(cached_data)
+            return deserialized_data
 
-            data = await fn()
-            serialized_data = serialize_to_json(data)
-            
-            await self.cache.set(key, serialized_data)
-            return data
-        except Exception:
-            import traceback
-            traceback.print_exc()
+        data = await fn()
+        serialized_data = serialize_to_json(data)
+        
+        await self.cache.set(key, serialized_data)
+        return data
+
     
     async def invalidate_cache(self, keys):
         for key in keys:
