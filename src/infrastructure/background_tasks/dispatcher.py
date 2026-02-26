7d6e32deb1915a7d3875223e66b18b813ddf732b
cdxy1 <dalyvinchi@gmail.com>
2026-02-27 01:40:38 +0300

    Refactor background tasks

--- src/infrastructure/background_tasks/dispatcher.py
+++ src/infrastructure/background_tasks/dispatcher.py
@@ -2,8 +2,8 @@ from src.infrastructure.background_tasks.app import celery_app
 
 
 class TasksDispatcher:
-    def dispatch_upload_file(self, file_key, file_data):
+    def dispatch_upload_file(self, prefix, file_key, file_data):
         celery_app.send_task(
             "tasks.upload_file",
-            args=[file_key, file_data]
+            args=[prefix, file_key, file_data]
         )
--- src/infrastructure/background_tasks/tasks/tasks.py
+++ src/infrastructure/background_tasks/tasks/tasks.py
@@ -1,18 +1,12 @@
-import asyncio
 from base64 import b64decode
 
-import src.infrastructure.models  #NOQA
-from src.application.dto.track_dto import UpdateTrackDTO
 from src.infrastructure.background_tasks.app import celery_app, storage
-from src.infrastructure.background_tasks.dependencies.track import (
-    get_update_track_usecase,
-)
-
+from src.infrastructure.background_tasks.utils.db import run_sql
 
 @celery_app.task(name="tasks.upload_file")
-def upload_file(file_key, file_data_b64):
+def upload_file(prefix, file_key, file_data_b64):    
     bytes_data = b64decode(file_data_b64)
-    storage.upload_file(str(file_key), bytes_data)
-    usecase = get_update_track_usecase()
+    full_file_key = f"{prefix}/{file_key}"
+    storage.upload_file(full_file_key, bytes_data)
     
-    asyncio.run(usecase(file_key, UpdateTrackDTO(audio_key=file_key)))
+    run_sql("""UPDATE track SET audio_key=%s WHERE track_id=%s::UUID""", (full_file_key, str(file_key)))
