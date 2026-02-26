e7b7366e1b1884df70fe97e1f153f081c3239b1d
cdxy1 <dalyvinchi@gmail.com>
2026-02-27 01:26:16 +0300

    Add sync uri for postgres

--- src/infrastructure/config/postgres.py
+++ src/infrastructure/config/postgres.py
@@ -13,3 +13,13 @@ class PostgresConfig(IDatabaseConfig):
         db_name = os.environ.get("DB_NAME")
         
         return f"postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+    @property
+    def sync_database_uri(self) -> str:
+        db_host = os.environ.get("DB_HOST")
+        db_port = os.environ.get("DB_PORT")
+        db_user = os.environ.get("DB_USER")
+        db_password = os.environ.get("DB_PASSWORD")
+        db_name = os.environ.get("DB_NAME")
+        
+        return f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"      
