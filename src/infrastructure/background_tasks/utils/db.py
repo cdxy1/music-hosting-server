098ca7ea3b0ed5581f4079b2929422b069fd2e49
cdxy1 <dalyvinchi@gmail.com>
2026-02-27 01:20:12 +0300

    Add sql query runner for celery

---
+++ src/infrastructure/background_tasks/utils/db.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+from src.infrastructure.config.postgres import PostgresConfig
+
+def run_sql(query, args):
+    uri = PostgresConfig().sync_database_uri
+    with psycopg2.connect(uri) as conn:
+        with conn.cursor() as cur:
+            
+            if args:
+                cur.execute(query, args)
+            else:
+                cur.execute(query)
+            
+            if cur.description:
+                return cur.fetchall()
