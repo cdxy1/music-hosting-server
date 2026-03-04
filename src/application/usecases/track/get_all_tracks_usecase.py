f3a37c05146cbf63aa7c8becd733c84f6ca8a3e2
cdxy1 <dalyvinchi@gmail.com>
2026-03-05 00:57:33 +0300

    Add generate url to track usecase

--- src/application/usecases/track/get_all_tracks_usecase.py
+++ src/application/usecases/track/get_all_tracks_usecase.py
@@ -1,11 +1,25 @@
+from typing import override
+
 from src.application.dto.track_dto import TrackOutputDTO
 from src.application.usecases.base import BaseUsecase
 
 
 class GetAllTracksUsecase(BaseUsecase):
+    @override
+    def __init__(self, repo, uow_factory, cache, file_storage):
+        super().__init__(repo, uow_factory, cache)
+        self.file_storage = file_storage    
+    
     async def __call__(self):
         uow = self.uow_factory()
         async with uow() as session:
             tracks = await self.cache.get_or_create("tracks:all", lambda: self.repo.get_all(session))
             
-            return tuple(TrackOutputDTO(**track.to_dict()) for track in tracks)
+            tracks_with_url = []
+            for track in tracks:
+                track_url = self.file_storage.get_file_url(track.audio_key) if track.audio_key else None
+                
+                track_with_url = track.to_dict_with_url(track_url)
+                tracks_with_url.append(track_with_url)
+            
+            return tuple(TrackOutputDTO(**track) for track in tracks_with_url)
--- src/application/usecases/track/get_track_usecase.py
+++ src/application/usecases/track/get_track_usecase.py
@@ -1,13 +1,21 @@
 from uuid import UUID
+from typing import override
 
 from src.application.dto.track_dto import TrackOutputDTO
 from src.application.usecases.base import BaseUsecase
 
 
 class GetTrackUsecase(BaseUsecase): 
+    @override
+    def __init__(self, repo, uow_factory, cache, file_storage):
+        super().__init__(repo, uow_factory, cache)
+        self.file_storage = file_storage
+    
     async def __call__(self, track_id: UUID):
         uow = self.uow_factory()
         async with uow() as session:
             track = await self.cache.get_or_create(f"tracks:{track_id}", lambda: self.repo.get_by_id(session, track_id))
+            track_url = self.file_storage.get_file_url(track.audio_key) if track.audio_key else None
+            track_with_url = track.to_dict_with_url(track_url)
             
-            return TrackOutputDTO(**track.to_dict())
+            return TrackOutputDTO(**track_with_url)
