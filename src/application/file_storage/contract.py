27bbd9855bbf344e3730b15a4cd416648be89ff8
cdxy1 <dalyvinchi@gmail.com>
2026-03-05 00:56:34 +0300

    Rename method

--- src/application/file_storage/contract.py
+++ src/application/file_storage/contract.py
@@ -2,5 +2,5 @@ from abc import ABC, abstractmethod
 
 class IFileStorage(ABC):
     @abstractmethod
-    async def get_url(self, key: str) -> str:
+    def get_file_url(self, key: str) -> str:
         pass
