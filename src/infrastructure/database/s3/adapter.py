1c33bc8e96b8cc27eda9aba34a974178f46b30dc
cdxy1 <dalyvinchi@gmail.com>
2026-03-05 00:59:17 +0300

    Add s3 adapter

---
+++ src/infrastructure/database/s3/adapter.py
@@ -0,0 +1,8 @@
+from src.application.file_storage.contract import IFileStorage
+
+class S3Adapter(IFileStorage):
+    def __init__(self, s3_storage):
+        self.s3_storage = s3_storage
+        
+    def get_file_url(self, key: str) -> str:
+        return self.s3_storage.create_presigned_url(key)
