d051a8b0d9925287241df4ad4f0f8ef444267fbe
cdxy1 <dalyvinchi@gmail.com>
2026-03-08 04:52:27 +0300

    Add upload image to tasks

--- src/infrastructure/background_tasks/tasks/tasks.py
+++ src/infrastructure/background_tasks/tasks/tasks.py
@@ -3,10 +3,14 @@ from base64 import b64decode
 from src.infrastructure.background_tasks.app import celery_app, storage
 from src.infrastructure.background_tasks.utils.db import run_sql
 
+
 @celery_app.task(name="tasks.upload_file")
 def upload_file(prefix, file_key, file_data_b64):    
     bytes_data = b64decode(file_data_b64)
     full_file_key = f"{prefix}/{file_key}"
     storage.upload_file(full_file_key, bytes_data)
     
-    run_sql("""UPDATE track SET audio_key=%s WHERE track_id=%s::UUID""", (full_file_key, str(file_key)))
+    if prefix == "tracks":
+        run_sql("""UPDATE track SET audio_key=%s WHERE track_id=%s::UUID""", (full_file_key, str(file_key)))
+    elif prefix == "images":
+        run_sql("""UPDATE release SET image_key=%s WHERE release_id=%s::UUID""", (full_file_key, str(file_key)))
